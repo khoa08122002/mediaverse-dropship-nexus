6cccd17187020ea21955dc48ac28faeb9722398d
khoa08122002 <80825266+khoa08122002@users.noreply.github.com>
2025-06-19 08:41:56 +0700

    Hey

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const { NestFactory } = require('@nestjs/core');
+const { AppModule } = require('../dist/backend/app.module');
+const cookieParser = require('cookie-parser');
+
+let app;
+
+async function createApp() {
+  if (!app) {
+    app = await NestFactory.create(AppModule, {
+      logger: ['error', 'warn', 'log'],
+    });
+
+    const corsOrigins = process.env.NODE_ENV === 'production'
+      ? ['https://phg2.vercel.app']
+      : ['http://localhost:3000', 'http://localhost:5173'];
+
+    app.enableCors({
+      origin: corsOrigins,
+      credentials: true,
+    });
+
+    app.useGlobalPipes(new (require('@nestjs/common').ValidationPipe)({
+      transform: true,
+      whitelist: true,
+    }));
+
+    app.use(cookieParser());
+    app.setGlobalPrefix('api');
+
+    await app.init();
+  }
+  return app;
+}
+
+module.exports = async (req, res) => {
+  const app = await createApp();
+  const httpAdapter = app.getHttpAdapter();
+  const instance = httpAdapter.getInstance();
+  
+  return instance(req, res);
+}; 
